e68a437323907caa71b25423afed54a047de6e41
ScottDeev <amehelijah99@gmail.com>
2022-12-31 19:30:42 +0100

    Feat: Set up project folder structure with sass

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="bg-pending-light-9"
+          className="bg-pending-light-9 Avenir"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
